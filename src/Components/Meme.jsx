b034ed8f0ead491883243bbc4b84d13b671fd2f6
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-08-06 18:20:03 +0100

    altered array

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,14 +1,36 @@
+import { useState } from 'react'
 
-
+import memesData from '../memesData.js'
 
 function Meme() {
+
+    // const [memeImage, setMemeImage] = useState("")
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+
+    function getMemeImage () {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
+
   return (
     <main>
-        <form className="form">
+        <div className="form">
             <input type="text" className="form-input" placeholder="Top text"/>
             <input type="text" className="form-input" placeholder="Bottom text"/>
-            <button type="submit" className="form-btn">Generate meme</button>
-        </form>
+            <button type="button" onClick={getMemeImage} className="form-btn">Generate meme</button>
+        </div>
+        <img src={meme.randomImage} className='meme-image' alt="" />
     </main>
   )
 }
